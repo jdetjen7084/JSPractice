7668f71ac9488af111468e75a78c7817b60935ee
Justin Detjen <jdetjen@gmx.com>
2020-01-06 17:41:59 -0600

    Refactor calculate functions to use bind

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -27,56 +27,77 @@ function writeToLog(
   console.log(logEntries);
 }
 
-function calculateResult(calculationType) {
-    const enteredNumber = getUserInput();
-  if (
-    calculationType !== "ADD" &&
-    calculationType !== "SUBTRACT" &&
-    calculationType !== "MULTIPLY" &&
-    calculationType !== "DIVIDE" ||
-    !enteredNumber
-  ) {
-    return;
+// function calculateResult(calculationType) {
+//     const enteredNumber = getUserInput();
+//   if (
+//     calculationType !== "ADD" &&
+//     calculationType !== "SUBTRACT" &&
+//     calculationType !== "MULTIPLY" &&
+//     calculationType !== "DIVIDE" ||
+//     !enteredNumber
+//   ) {
+//     return;
+//   }
+//     const enteredNumber = getUserInput();
+//     const initialResult = currentResult;
+//     let mathOperator;
+//     if (calculationType === "add") {
+//       currentResult += enteredNumber;
+//       mathOperator = "+";
+//     } else if (calculationType === "SUBTRACT") {
+//       currentResult -= enteredNumber;
+//       mathOperator = "-";
+//     } else if (calculationType === "MULTIPLY") {
+//       currentResult *= enteredNumber;
+//       mathOperator = "*";
+//     } else if (calculationType === "DIVIDE") {
+//       currentResult /= enteredNumber;
+//       mathOperator = "/";
+//     }
+//     createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+//     writeToLog(initialResult, enteredNumber, currentResult);
+//   }
+
+
+
+// function add() {
+//   calculateResult("ADD");
+// }
+
+// function subtract() {
+//   calculateResult("SUBTRACT");
+// }
+
+// function multiply() {
+//   calculateResult("MULTIPLY");
+// }
+
+// function divide() {
+//   calculateResult("DIVIDE");
+// }
+
+function calculate(operation) {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  let operator;
+  if (operation === 'ADD') {
+    currentResult += enteredNumber;
+    operator = '+';
+  } else if (operation === 'SUBTRACT') {
+    currentResult -= enteredNumber;
+    operator = '-';
+  } else if (operation === 'MULTIPLY') {
+    currentResult *= enteredNumber;
+    operator = '*';
+  } else {
+    currentResult /= enteredNumber;
+    operator = '/';
   }
-    const enteredNumber = getUserInput();
-    const initialResult = currentResult;
-    let mathOperator;
-    if (calculationType === "add") {
-      currentResult += enteredNumber;
-      mathOperator = "+";
-    } else if (calculationType === "SUBTRACT") {
-      currentResult -= enteredNumber;
-      mathOperator = "-";
-    } else if (calculationType === "MULTIPLY") {
-      currentResult *= enteredNumber;
-      mathOperator = "*";
-    } else if (calculationType === "DIVIDE") {
-      currentResult /= enteredNumber;
-      mathOperator = "/";
-    }
-    createAndWriteOutput(mathOperator, initialResult, enteredNumber);
-    writeToLog(initialResult, enteredNumber, currentResult);
-  }
-
-
-
-function add() {
-  calculateResult("ADD");
+  createAndWriteOutput(operator, initialResult, enteredNumber);
+  writeToLog(operation, initialResult, enteredNumber, currentResult);
 }
 
-function subtract() {
-  calculateResult("SUBTRACT");
-}
-
-function multiply() {
-  calculateResult("MULTIPLY");
-}
-
-function divide() {
-  calculateResult("DIVIDE");
-}
-
-addBtn.addEventListener("click", add);
-subtractBtn.addEventListener("click", subtract);
-multiplyBtn.addEventListener("click", multiply);
-divideBtn.addEventListener("click", divide);
+addBtn.addEventListener("click", calculate.bind(this, 'ADD'));
+subtractBtn.addEventListener("click", calculate.bind(this, 'SUBTRACT'));
+multiplyBtn.addEventListener("click", calculate.bind(this, 'MULTIPLY'));
+divideBtn.addEventListener("click", calculate.bind(this, 'DIVIDE'));
